4976d211a35d3bcf13c2d709e84283666536f295
Stephen Seagrave <sseagrave@oneviewhealthcare.com>
2021-09-24 11:44:26 +0100

    Add project files.

---
+++ MatchingPairs/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace MatchingPairs
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        private List<string> icons = new List<string>()
+        {
+            "!", "!", "N", "N", ",", ",", "k", "k",
+            "b", "b", "v", "v", "w", "w", "z", "z" 
+
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            AssignIconsToSquares();
+        }
+
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
